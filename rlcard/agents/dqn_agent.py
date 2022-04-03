8ed794523f9d2427024bf4b42bdcff9745019a12
Yichen Liu <55074004+lyc1005@users.noreply.github.com>
2022-04-03 19:59:28 +0800

    Update dqn_agent.py

--- rlcard/agents/dqn_agent.py
+++ rlcard/agents/dqn_agent.py
@@ -37,6 +37,35 @@ from rlcard.utils.utils import remove_illegal
 Transition = namedtuple('Transition', ['state', 'action', 'reward', 'next_state', 'legal_actions', 'done'])
 
 
+class NoisyFactorizedLinear(nn.Linear):
+    """
+    NoisyNet layer with factorized gaussian noise
+    N.B. nn.Linear already initializes weight and bias to
+    """
+    def __init__(self, in_features, out_features, sigma_zero=0.4, bias=True):
+        super(NoisyFactorizedLinear, self).__init__(in_features, out_features, bias=bias)
+        sigma_init = sigma_zero / math.sqrt(in_features)
+        self.sigma_weight = nn.Parameter(torch.Tensor(out_features, in_features).fill_(sigma_init))
+        self.register_buffer("epsilon_input", torch.zeros(1, in_features))
+        self.register_buffer("epsilon_output", torch.zeros(out_features, 1))
+        if bias:
+            self.sigma_bias = nn.Parameter(torch.Tensor(out_features).fill_(sigma_init))
+
+    def forward(self, input):
+        bias = self.bias
+        func = lambda x: torch.sign(x) * torch.sqrt(torch.abs(x))
+
+        with torch.no_grad():
+            torch.randn(self.epsilon_input.size(), out=self.epsilon_input)
+            torch.randn(self.epsilon_output.size(), out=self.epsilon_output)
+            eps_in = func(self.epsilon_input)
+            eps_out = func(self.epsilon_output)
+            noise_v = torch.mul(eps_in, eps_out).detach()
+        if bias is not None:
+            bias = bias + self.sigma_bias * eps_out.t()
+        return F.linear(input, self.weight + self.sigma_weight * noise_v, bias)
+
+    
 class DQNAgent(object):
     '''
     Approximate clone of rlcard.agents.dqn_agent.DQNAgent
@@ -360,9 +389,9 @@ class EstimatorNetwork(nn.Module):
         fc = [nn.Flatten()]
         fc.append(nn.BatchNorm1d(layer_dims[0]))
         for i in range(len(layer_dims)-1):
-            fc.append(nn.Linear(layer_dims[i], layer_dims[i+1], bias=True))
+            fc.append(NoisyFactorizedLinear(layer_dims[i], layer_dims[i+1], bias=True))
             fc.append(nn.Tanh())
-        fc.append(nn.Linear(layer_dims[-1], self.num_actions, bias=True))
+        fc.append(NoisyFactorizedLinear(layer_dims[-1], self.num_actions, bias=True))
         self.fc_layers = nn.Sequential(*fc)
 
     def forward(self, s):
